35ebecae7ef56aef1d1720ef94e711c25aa4492b
Wojciech Janaszek <trash.wjanaszek@gmail.com>
2021-07-01 11:58:30 +0200

    feat: generate structure for api-todo

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/api-todo'],
+  projects: [
+    '<rootDir>/apps/api-todo',
+    '<rootDir>/libs/api-todo/domain',
+    '<rootDir>/libs/api-todo/infrastructure',
+    '<rootDir>/libs/api-todo/application',
+    '<rootDir>/libs/api-todo/shell',
+  ],
 };
---
+++ libs/api-todo/application/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/api-todo-application.module';
