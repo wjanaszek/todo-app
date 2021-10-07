5e75dc15aa34d6d1860f76cf272018f9a33fe556
Wojciech Janaszek <trash.wjanaszek@gmail.com>
2021-10-07 13:52:15 +0200

    feat: add swagger

--- apps/api-todo/src/main.ts
+++ apps/api-todo/src/main.ts
@@ -1,21 +1,42 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+
+const GLOBAL_PREFIX = 'api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+
+  setupPrefix(app);
+  setupSwagger(app);
+
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port + '/' + GLOBAL_PREFIX);
   });
 }
 
 bootstrap();
+
+function setupPrefix(app: INestApplication): void {
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+}
+
+function setupSwagger(app: INestApplication): void {
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const config = new DocumentBuilder()
+    .setTitle('Todo API')
+    .setDescription('The todo API description')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('swagger', app, document);
+}
--- libs/api-todo/infrastructure/src/index.ts
+++ libs/api-todo/infrastructure/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/api-todo-infrastructure.module';
 
 export * from './lib/dto/create-todo.dto';
+export * from './lib/dto/todo.dto';
 export * from './lib/dto/update-todo.dto';
 
 export * from './lib/orm/psql/psql-todo.entity';
