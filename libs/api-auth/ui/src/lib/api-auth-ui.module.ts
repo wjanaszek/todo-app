fa877a37f33d345a05a796a1130330ffd30a4321
Wojciech Janaszek <trash.wjanaszek@gmail.com>
2021-10-07 11:56:04 +0200

    feat: add logging middleware

--- libs/api-auth/ui/src/lib/api-auth-ui.module.ts
+++ libs/api-auth/ui/src/lib/api-auth-ui.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ApiAuthShellModule } from '@wjanaszek/api-auth/shell';
+import {
+  RestLoggingMiddleware,
+  RestLoggingMiddlewareModule,
+} from '@wjanaszek/shared/infrastructure';
 import { RestApiAuthController } from './rest-api-auth.controller';
 
 @Module({
-  imports: [ApiAuthShellModule],
+  imports: [ApiAuthShellModule, RestLoggingMiddlewareModule.forRoot()],
   controllers: [RestApiAuthController],
 })
-export class ApiAuthUiModule {}
+export class ApiAuthUiModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(RestLoggingMiddleware).forRoutes(RestApiAuthController.URI);
+  }
+}
--- libs/shared/infrastructure/src/index.ts
+++ libs/shared/infrastructure/src/index.ts
@@ -5,3 +5,5 @@ export * from './lib/exceptions/http-not-found.exception';
 export * from './lib/injection-tokens/database-connection.injection-token';
 
 export * from './lib/databases/database.module'
+
+export * from './lib/middlewares/rest/rest-logging.middleware';
